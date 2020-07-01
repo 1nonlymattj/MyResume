bdb2f1b33cb8a374b858e4b1847eafb681fbd789
Matthew Johnson <matthew.johnson.185.ctr@us.af.mil>
2020-07-01 11:57:25 -0500

    updated resume

--- WebContent/js/resume.js
+++ WebContent/js/resume.js
@@ -1,28 +1,40 @@
 (function($) {
-  "use strict"; // Start of use strict
+   "use strict"; // Start of use strict
 
-  // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top)
-        }, 1000, "easeInOutExpo");
-        return false;
+   $(".inner-switch").on("click", function() {
+      if ($("body").hasClass("dark")) {
+         $("body").removeClass("dark");
+         $(".inner-switch").text("OFF");
+      } else {
+         $("body").addClass("dark");
+         $(".inner-switch").text("ON");
       }
-    }
-  });
+   });
 
-  // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
-  });
+   // Smooth scrolling using jQuery easing
+   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(
+         function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+                  && location.hostname == this.hostname) {
+               var target = $(this.hash);
+               target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+               if (target.length) {
+                  $('html, body').animate({
+                     scrollTop : (target.offset().top)
+                  }, 1000, "easeInOutExpo");
+                  return false;
+               }
+            }
+         });
 
-  // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#sideNav'
-  });
+   // Closes responsive menu when a scroll trigger link is clicked
+   $('.js-scroll-trigger').click(function() {
+      $('.navbar-collapse').collapse('hide');
+   });
+
+   // Activate scrollspy to add active class to navbar items on scroll
+   $('body').scrollspy({
+      target : '#sideNav'
+   });
 
 })(jQuery); // End of use strict
